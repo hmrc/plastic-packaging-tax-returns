6644d21143e03a4db9a14038afee29549fa45f14
Tomasz Bartosiewicz <2415677+tombart@users.noreply.github.com>
2021-03-15 17:15:48 +0000

    PPT-446: initial tax return repo wiring

--- app/uk/gov/hmrc/plasticpackagingtaxreturns/controllers/ReturnsController.scala
+++ app/uk/gov/hmrc/plasticpackagingtaxreturns/controllers/ReturnsController.scala
@@ -16,18 +16,76 @@
 
 package uk.gov.hmrc.plasticpackagingtaxreturns.controllers
 
+import play.api.Logger
+import play.api.libs.json._
 import play.api.mvc.{ControllerComponents, _}
+import uk.gov.hmrc.plasticpackagingtaxreturns.controllers.response.JSONResponses
+import uk.gov.hmrc.plasticpackagingtaxreturns.models.{TaxReturn, TaxReturnRequest}
+import uk.gov.hmrc.plasticpackagingtaxreturns.repositories.TaxReturnRepository
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+import uk.gov.hmrc.play.bootstrap.backend.http.ErrorResponse
 
 import javax.inject.Inject
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
-class ReturnsController @Inject() (override val controllerComponents: ControllerComponents)
-    extends BackendController(controllerComponents) {
+class ReturnsController @Inject() (
+  taxReturnRepository: TaxReturnRepository,
+  override val controllerComponents: ControllerComponents
+)(implicit executionContext: ExecutionContext)
+    extends BackendController(controllerComponents) with JSONResponses {
 
-  def get(): Action[AnyContent] =
-    Action.async { implicit request =>
-      Future.successful(Ok("OK"))
+  private val logger = Logger(this.getClass)
+
+  def parsingJson[T](implicit rds: Reads[T]): BodyParser[T] =
+    parse.json.validate { json =>
+      json.validate[T] match {
+        case JsSuccess(value, _) => Right(value)
+        case JsError(error) =>
+          val errorResponse = Json.toJson(ErrorResponse(BAD_REQUEST, "Bad Request"))
+          logger.warn(s"Bad Request [$errorResponse]")
+          logger.warn(s"Errors: [$error]")
+          Left(BadRequest(errorResponse))
+      }
     }
+
+  def get(id: String): Action[AnyContent] =
+    Action.async { implicit request =>
+      taxReturnRepository.findById(id).map {
+        case Some(taxReturn) => Ok(taxReturn)
+        case None            => NotFound
+      }
+    }
+
+  def create(): Action[TaxReturnRequest] =
+    Action.async(parsingJson[TaxReturnRequest]) { implicit request =>
+      logPayload("Create Tax Return Request Received", request.body)
+      taxReturnRepository
+        .create(request.body.toTaxReturn("123"))
+        .map(logPayload("Create Tax Return Response", _))
+        .map(taxReturn => Created(taxReturn))
+    }
+
+  def update(id: String): Action[TaxReturnRequest] =
+    Action.async(parsingJson[TaxReturnRequest]) { implicit request =>
+      logPayload("Update Tax Return Request Received", request.body)
+      taxReturnRepository.findById(id).flatMap {
+        case Some(_) =>
+          taxReturnRepository
+            .update(request.body.toTaxReturn(id))
+            .map(logPayload("Update Tax Return Response", _))
+            .map {
+              case Some(taxReturn) => Ok[TaxReturn](taxReturn)
+              case None            => NotFound
+            }
+        case None =>
+          logPayload("Update Tax Return Response", "Not Found")
+          Future.successful(NotFound)
+      }
+    }
+
+  private def logPayload[T](prefix: String, payload: T)(implicit wts: Writes[T]): T = {
+    logger.debug(s"$prefix, Payload: ${Json.toJson(payload)}")
+    payload
+  }
 
 }
---
+++ test/uk/gov/hmrc/plasticpackagingtaxreturns/controllers/repositories/TaxReturnRepositoryItSpec.scala
@@ -0,0 +1,147 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.plasticpackagingtaxreturns.controllers.repositories
+
+import com.codahale.metrics.{MetricFilter, SharedMetricRegistries, Timer}
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import play.api.inject.guice.GuiceApplicationBuilder
+import reactivemongo.api.ReadConcern
+import uk.gov.hmrc.plasticpackagingtaxreturns.controllers.builders.TaxReturnBuilder
+import uk.gov.hmrc.plasticpackagingtaxreturns.models.TaxReturn
+import uk.gov.hmrc.plasticpackagingtaxreturns.repositories.TaxReturnRepository
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class TaxReturnRepositoryItSpec
+    extends AnyWordSpec with Matchers with ScalaFutures with BeforeAndAfterEach with IntegrationPatience
+    with TaxReturnBuilder {
+
+  private val injector = {
+    SharedMetricRegistries.clear()
+    GuiceApplicationBuilder().injector()
+  }
+
+  private val repository = injector.instanceOf[TaxReturnRepository]
+
+  override def beforeEach(): Unit = {
+    super.beforeEach()
+    SharedMetricRegistries.clear()
+    repository.removeAll().futureValue
+  }
+
+  private def collectionSize: Int =
+    repository.collection
+      .count(selector = None, limit = Some(0), skip = 0, hint = None, readConcern = ReadConcern.Local)
+      .futureValue
+      .toInt
+
+  private def gienTaxReturnExists(taxReturns: TaxReturn*): Unit =
+    repository.collection.insert(ordered = true).many(taxReturns).futureValue
+
+  "Create" should {
+    "persist the tax return" in {
+      val taxReturn = aTaxReturn()
+      repository.create(taxReturn).futureValue mustBe taxReturn
+
+      collectionSize mustBe 1
+    }
+  }
+
+  "Update" should {
+    "update the tax return" in {
+      val taxReturn = aTaxReturn(withManufacturedPlasticWeight(totalKg = Some(5), totalKgBelowThreshold = Some(2)))
+      gienTaxReturnExists(taxReturn)
+
+      repository.update(taxReturn).futureValue mustBe Some(taxReturn)
+
+      // this indicates that a timer has started and has been stopped
+      getTimer("mongo.taxReturn.update").getCount mustBe 1
+
+      collectionSize mustBe 1
+    }
+
+    "do nothing for missing tax return" in {
+      val taxReturn = aTaxReturn()
+
+      repository.update(taxReturn).futureValue mustBe None
+
+      collectionSize mustBe 0
+
+      getTimer("mongo.taxReturn.update").getCount mustBe 1
+    }
+  }
+
+  "Find by ID" should {
+    "return the persisted tax return" when {
+      "one exists with ID" in {
+        val taxReturn = aTaxReturn()
+        gienTaxReturnExists(taxReturn)
+
+        repository.findById(taxReturn.id).futureValue mustBe Some(taxReturn)
+
+        // this indicates that a timer has started and has been stopped
+        getTimer("mongo.taxReturn.find").getCount mustBe 1
+      }
+    }
+
+    "return None" when {
+      "none exist with id" in {
+        val taxReturn1 = aTaxReturn(withId("some-other-id"))
+        val taxReturn2 = aTaxReturn()
+        gienTaxReturnExists(taxReturn1, taxReturn2)
+
+        repository.findById("non-existing-id").futureValue mustBe None
+
+        getTimer("mongo.taxReturn.find").getCount mustBe 1
+      }
+    }
+  }
+
+  "Delete" should {
+    "remove the tax return" in {
+      val taxReturn = aTaxReturn()
+      gienTaxReturnExists(taxReturn)
+
+      repository.delete(taxReturn).futureValue
+
+      collectionSize mustBe 0
+    }
+
+    "maintain other tax return" when {
+      "they have a different ID" in {
+        val taxReturn1 = aTaxReturn()
+        val taxReturn2 = aTaxReturn(withId("id1"))
+        val taxReturn3 = aTaxReturn(withId("id2"))
+        gienTaxReturnExists(taxReturn2, taxReturn3)
+
+        repository.delete(taxReturn1).futureValue
+
+        collectionSize mustBe 2
+      }
+    }
+  }
+
+  private def getTimer(name: String): Timer =
+    SharedMetricRegistries
+      .getOrCreate("plastic-packaging-tax-returns")
+      .getTimers(MetricFilter.startsWith(name))
+      .get(name)
+
+}
