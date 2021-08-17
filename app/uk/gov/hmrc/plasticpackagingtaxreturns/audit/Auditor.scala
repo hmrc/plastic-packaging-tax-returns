347c417c12cd3a1514dbbc74fb538bdbf9c55b30
kiranprakashdigital <74252533+kiranprakashdigital@users.noreply.github.com>
2021-08-17 09:58:34 +0100

    PPTP-1011 Added audit event for subscrpition update (#26)

---
+++ app/uk/gov/hmrc/plasticpackagingtaxreturns/audit/Auditor.scala
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.plasticpackagingtaxreturns.audit
+
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.plasticpackagingtaxreturns.connectors.models.eis.subscriptionUpdate.SubscriptionUpdateRequest
+import uk.gov.hmrc.play.audit.http.connector.AuditConnector
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.ExecutionContext
+
+@Singleton
+class Auditor @Inject() (auditConnector: AuditConnector) {
+
+  def subscriptionUpdated(
+    subscriptionUpdateRequest: SubscriptionUpdateRequest,
+    pptReference: Option[String] = None
+  )(implicit hc: HeaderCarrier, ex: ExecutionContext) =
+    auditConnector.sendExplicitAudit(ChangeSubscriptionEvent.eventType,
+                                     ChangeSubscriptionEvent(subscriptionUpdateRequest, pptReference)
+    )
+
+}
--- build.sbt
+++ build.sbt
@@ -38,7 +38,7 @@ lazy val scoverageSettings: Seq[Setting[_]] = Seq(
     , "metrics\\..*"
     , ".*(BuildInfo|Routes|Options).*"
   ).mkString(";"),
-  coverageMinimum := 93,
+  coverageMinimum := 97,
   coverageFailOnMinimum := true,
   coverageHighlighting := true,
   parallelExecution in Test := false
