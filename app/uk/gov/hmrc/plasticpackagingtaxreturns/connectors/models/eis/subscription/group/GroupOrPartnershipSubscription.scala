1ac9112f398d9606a621a11b405fcfc3e116e8c1
kiranprakashdigital <74252533+kiranprakashdigital@users.noreply.github.com>
2021-09-15 16:09:16 +0100

    PPTP-1124 - Changed name GroupOrPartnershipSubscription

--- app/uk/gov/hmrc/plasticpackagingtaxreturns/connectors/models/eis/subscription/group/GroupOrPartnershipSubscription.scala
+++ app/uk/gov/hmrc/plasticpackagingtaxreturns/connectors/models/eis/subscription/group/GroupOrPartnershipSubscription.scala
@@ -20,7 +20,7 @@ import play.api.libs.json.{Json, OFormat}
 
 case class GroupOrPartnershipSubscription(
   representativeControl: Boolean,
-  groupDetails: GroupDetails,
+  groupPartnershipDetails: GroupOrPartnershipDetails,
   allMembersControl: Boolean
 )
 
