6243c3b22de7478c06217abd20411ca0b22035f8
Steve Sugden <46934286+SteveSugden@users.noreply.github.com>
2021-09-23 09:00:39 +0100

    PPTP-1210 - Update application library dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,12 +5,12 @@ import sbt._
 
 object AppDependencies {
 
-  val compile = Seq("uk.gov.hmrc" %% "bootstrap-backend-play-28" % "5.2.0",
+  val compile = Seq("uk.gov.hmrc" %% "bootstrap-backend-play-28" % "5.14.0",
                     "uk.gov.hmrc"       %% "simple-reactivemongo" % "8.0.0-play-28",
                     "com.typesafe.play" %% "play-json-joda"       % "2.6.14"
   )
 
-  val test = Seq("uk.gov.hmrc" %% "bootstrap-test-play-28" % "5.2.0" % Test,
+  val test = Seq("uk.gov.hmrc" %% "bootstrap-test-play-28" % "5.14.0" % Test,
                  "org.scalatest"          %% "scalatest"             % "3.2.5"    % Test,
                  "com.typesafe.play"      %% "play-test"             % current    % Test,
                  "org.mockito"             % "mockito-core"          % "3.9.0"    % Test,
