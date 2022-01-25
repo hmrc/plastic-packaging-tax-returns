bcd0f1077eb54956e38d647bd6efca38ffe6ecca
Mauro Fanton <maurofanton@c02fw0v8ml7h.home>
2022-01-25 12:16:43 +0000

    more unit test passed

--- app/uk/gov/hmrc/plasticpackagingtaxreturns/controllers/TileInfoController.scala
+++ app/uk/gov/hmrc/plasticpackagingtaxreturns/controllers/TileInfoController.scala
@@ -18,20 +18,33 @@ package uk.gov.hmrc.plasticpackagingtaxreturns.controllers
 
 import play.api.libs.json.Json
 import play.api.mvc.{Action, AnyContent, ControllerComponents, Result}
-import uk.gov.hmrc.plasticpackagingtaxreturns.connectors.models.des.enterprise.ObligationDataResponse
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.plasticpackagingtaxreturns.connectors.ObligationDataConnector
+import uk.gov.hmrc.plasticpackagingtaxreturns.connectors.models.des.enterprise.{ObligationDataResponse, ObligationStatus}
 import uk.gov.hmrc.plasticpackagingtaxreturns.models.PPTObligations
 import uk.gov.hmrc.plasticpackagingtaxreturns.services.PTPObligationsService
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 
+import java.time.LocalDate
 import javax.inject.Inject
 
-class TileInfoController @Inject()(
-                                    cc: ControllerComponents,
-                                    obligationsService: PTPObligationsService)
-  extends BackendController(cc) {
-
+class TileInfoController @Inject()
+(
+  cc: ControllerComponents,
+  obligationDataConnector: ObligationDataConnector,
+  obligationsService: PTPObligationsService
+)
+extends BackendController(cc) {
 
   def get(ref: String): Action[AnyContent] = Action {
+    implicit val hc: HeaderCarrier =  HeaderCarrier()
+
+    val data = obligationDataConnector.get(
+      ref,
+      LocalDate.of(2022, 4,1),
+      LocalDate.now(),
+      ObligationStatus.OPEN )
+
     Ok(Json.toJson(obligationsService.get(ObligationDataResponse(Seq.empty))))
   }
 
--- test/uk/gov/hmrc/plasticpackagingtaxreturns/controllers/controllers/TileInfoControllerSpec.scala
+++ test/uk/gov/hmrc/plasticpackagingtaxreturns/controllers/controllers/TileInfoControllerSpec.scala
@@ -16,39 +16,44 @@
 
 package uk.gov.hmrc.plasticpackagingtaxreturns.controllers.controllers
 
-import org.mockito.ArgumentMatchers.any
-import play.api.inject.bind
+import org.mockito.ArgumentMatchers.{any, eq => exactlyEq}
 import org.mockito.Mockito.{reset, verify, when}
 import org.scalatest.BeforeAndAfterEach
 import org.scalatestplus.mockito.MockitoSugar
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.Application
+import play.api.inject.bind
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.libs.json.Json
-import play.api.mvc.{ControllerComponents, Result}
+import play.api.mvc.Result
 import play.api.test.Helpers.{OK, contentAsJson, defaultAwaitTimeout, route, status, _}
 import play.api.test.{FakeRequest, Injecting}
-import uk.gov.hmrc.plasticpackagingtaxreturns.connectors.models.des.enterprise.ObligationDataResponse
+import uk.gov.hmrc.plasticpackagingtaxreturns.connectors.ObligationDataConnector
+import uk.gov.hmrc.plasticpackagingtaxreturns.connectors.models.des.enterprise.{ObligationDataResponse, ObligationStatus}
 import uk.gov.hmrc.plasticpackagingtaxreturns.controllers.TileInfoController
 import uk.gov.hmrc.plasticpackagingtaxreturns.models.PPTObligations
 import uk.gov.hmrc.plasticpackagingtaxreturns.services.PTPObligationsService
 
+import java.time.LocalDate
 import java.util.UUID
 import scala.concurrent.Future
 
-class TileInfoControllerSpec extends PlaySpec with BeforeAndAfterEach with MockitoSugar with GuiceOneAppPerSuite with Injecting {
+class TileInfoControllerSpecTileInfoControllerSpec extends PlaySpec with BeforeAndAfterEach with MockitoSugar with GuiceOneAppPerSuite with Injecting {
   val mockPTPObligations: PTPObligationsService = mock[PTPObligationsService]
+  val mockObligationDataConnector = mock[ObligationDataConnector]
   lazy val sut: TileInfoController = inject[TileInfoController]
   val testPPTReference: String = UUID.randomUUID().toString
 
   override def beforeEach(): Unit = {
     super.beforeEach()
-    reset(mockPTPObligations)
+    reset(mockPTPObligations, mockObligationDataConnector)
   }
 
   override lazy val app: Application = GuiceApplicationBuilder()
-    .overrides(bind[PTPObligationsService].toInstance(mockPTPObligations))
+    .overrides(
+      bind[PTPObligationsService].toInstance(mockPTPObligations),
+      bind[ObligationDataConnector].toInstance(mockObligationDataConnector))
     .build()
 
   "get" must {
@@ -72,6 +77,26 @@ class TileInfoControllerSpec extends PlaySpec with BeforeAndAfterEach with Mocki
       contentAsJson(result) mustBe Json.toJson(obligations)
       status(result) mustBe OK
     }
+
+    "get should call Obligation Connector" in {
+
+      val response = ObligationDataResponse(Seq.empty)
+      when(mockObligationDataConnector.get(any(), any(), any(), any())(any()))
+        .thenReturn(Future.successful(Right(response)))
+      when(mockPTPObligations.get(any())).thenReturn(obligations)
+
+      val result: Future[Result] = sut.get(testPPTReference)(request)
+
+
+      verify(mockObligationDataConnector)
+        .get(exactlyEq(testPPTReference),
+          exactlyEq(LocalDate.of(2022, 4, 1)),
+          exactlyEq(LocalDate.now()),
+          exactlyEq(ObligationStatus.OPEN))(any())
+
+      verify(mockPTPObligations).get(response)
+    }
   }
 
 }
+
