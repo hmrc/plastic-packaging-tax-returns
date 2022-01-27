405dadfcc4d8e1cfc82b355917c94f2475522e70
Carl Thompson <carl.thompson@opencastsoftware.com>
2022-01-27 16:18:37 +0000

    PPT Obligations Service tests added

--- test/uk/gov/hmrc/plasticpackagingtaxreturns/services/PPTObligationsServiceSpec.scala
+++ test/uk/gov/hmrc/plasticpackagingtaxreturns/services/PPTObligationsServiceSpec.scala
@@ -29,39 +29,76 @@ import uk.gov.hmrc.plasticpackagingtaxreturns.models.PPTObligations
 import java.time.LocalDate
 
 class PPTObligationsServiceSpec extends PlaySpec {
+  val sut: PPTObligationsService = new PPTObligationsService()
+
+  def makeDataResponse(obligationDetail: ObligationDetail*): ObligationDataResponse =
+    ObligationDataResponse(
+      Seq(
+        Obligation(identification =
+                     Identification(incomeSourceType = "unused", referenceNumber = "unused", referenceType = "unused"),
+                   obligationDetails = obligationDetail
+        )
+      )
+    )
+
+  def makeDetail(fromDate: LocalDate): ObligationDetail =
+    ObligationDetail(status = ObligationStatus.OPEN,
+                     inboundCorrespondenceFromDate = fromDate,
+                     inboundCorrespondenceToDate = fromDate.plusMonths(1),
+                     inboundCorrespondenceDateReceived = fromDate,
+                     inboundCorrespondenceDueDate = fromDate.plusMonths(1).minusDays(1),
+                     periodKey = "#001"
+    )
 
   "get" must {
 
-
     "return no obligations" in {
-      val obligationService: PPTObligationsService       = new PPTObligationsService()
-      val obligationDataResponse: ObligationDataResponse = ObligationDataResponse(Seq())
 
-      obligationService.get(obligationDataResponse) mustBe PPTObligations(None)
+      val obligationDataResponse: ObligationDataResponse = makeDataResponse()
+
+      sut.get(obligationDataResponse) mustBe PPTObligations(None)
 
     }
-    "return one obligations" in {
-      val obligationService: PPTObligationsService = new PPTObligationsService()
-      val obligationDataResponse: ObligationDataResponse = ObligationDataResponse(
-        Seq(
-          Obligation(
-            identification =
-              Identification(incomeSourceType = "unused", referenceNumber = "unused", referenceType = "unused"),
-            obligationDetails = Seq(
-              ObligationDetail(status = ObligationStatus.OPEN,
-                               inboundCorrespondenceFromDate = LocalDate.parse("2021-10-01"),
-                               inboundCorrespondenceToDate = LocalDate.parse("2021-11-01"),
-                               inboundCorrespondenceDateReceived = LocalDate.parse("2021-10-01"),
-                               inboundCorrespondenceDueDate = LocalDate.parse("2021-10-31"),
-                               periodKey = "#001"
-              )
-            )
-          )
-        )
-      )
+    "return nextObligation" when {
+      "there is only one obligationDetail" in {
 
-     // obligationService.get(obligationDataResponse) mustBe PPTObligations(Some(Obligation))
+        val upcomingObligationDate = makeDetail(LocalDate.now())
+        val obligationDataResponse = makeDataResponse(upcomingObligationDate)
 
+        sut.get(obligationDataResponse) mustBe PPTObligations(Some(upcomingObligationDate))
+
+      }
+      "there is an upcoming obligation and an overdue obligation" in {
+
+        val upcomingObligationDate = makeDetail(LocalDate.now())
+        val overdueObligationDate = makeDetail(LocalDate.now().minusMonths(1))
+        val obligationDataResponse = makeDataResponse(overdueObligationDate,upcomingObligationDate)
+
+        sut.get(obligationDataResponse) mustBe PPTObligations(Some(upcomingObligationDate))
+
+      }
+      "there are two upcoming obligations" in {
+
+        val upcomingObligationDate = makeDetail(LocalDate.now())
+        val laterObligationDate = makeDetail(LocalDate.now().plusMonths(1))
+        val obligationDataResponse = makeDataResponse(laterObligationDate,upcomingObligationDate)
+
+
+        withClue("obligation should be the soonest") {
+          sut.get(obligationDataResponse) mustBe PPTObligations(Some(upcomingObligationDate))
+          //todo assertion that received obligation is the next date after current
+        }
+      }
+      "there are a bunch of obligations" in {
+        val upcomingObligationDate = makeDetail(LocalDate.now())
+        val overdueObligationDate = makeDetail(LocalDate.now().minusMonths(1))
+        val laterObligationDate = makeDetail(LocalDate.now().plusMonths(1))
+
+        val obligationDataResponse = makeDataResponse(laterObligationDate,upcomingObligationDate,overdueObligationDate)
+
+        sut.get(obligationDataResponse) mustBe PPTObligations(Some(upcomingObligationDate))
+      }
     }
   }
+
 }
