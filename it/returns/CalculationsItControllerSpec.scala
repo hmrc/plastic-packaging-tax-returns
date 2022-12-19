d4dad0fc3d82a860dd1e457390e963bb9e0f4d9f
mycp98 <marcycalderon@hotmail.co.uk>
2022-12-19 16:42:14 +0000

    added integration tests

--- it/returns/CalculationsItControllerSpec.scala
+++ it/returns/CalculationsItControllerSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package returns
 
 import com.codahale.metrics.SharedMetricRegistries
@@ -9,7 +25,7 @@ import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach}
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneServerPerSuite
 import play.api.Application
-import play.api.http.Status.OK
+import play.api.http.Status.{OK, UNAUTHORIZED, UNPROCESSABLE_ENTITY}
 import play.api.inject.bind
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.libs.json.{JsObject, Json}
@@ -24,6 +40,7 @@ import uk.gov.hmrc.plasticpackagingtaxreturns.repositories.SessionRepository
 import uk.gov.hmrc.play.bootstrap.http.DefaultHttpClient
 
 import java.time.LocalDate
+import scala.concurrent
 import scala.concurrent.{ExecutionContext, Future}
 
 class CalculationsItControllerSpec
@@ -83,6 +100,26 @@ class CalculationsItControllerSpec
       val result = await(wsClient.url(s"http://localhost:$port/returns-calculate/$pptReference").get)
 
       result.status mustBe OK
+    }
+
+    "return unauthorised when calculating new return" in {
+      withUnauthorizedUser(new Exception)
+      stubGetBalanceRequest
+
+      val result = await(wsClient.url(s"http://localhost:$port/returns-calculate/$pptReference").get)
+
+      result.status mustBe UNAUTHORIZED
+    }
+
+    "return unprocessable entity when cache is invalid" in {
+      withAuthorizedUser()
+
+      when(sessionRepository.get(any)).thenReturn(Future.successful(None))
+
+      val result = await(wsClient.url(s"http://localhost:$port/returns-calculate/$pptReference").get)
+
+      result.status mustBe UNPROCESSABLE_ENTITY
+
     }
   }
 
