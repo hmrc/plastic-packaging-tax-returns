3f53f6603bb6cd4685c446b7fc8f7aed6db5f630
PSouthern <45816181+PSouthern@users.noreply.github.com>
2023-02-24 12:39:43 +0000

    Updating following platbot guidance

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,15 +3,18 @@ import sbt._
 
 object AppDependencies {
 
+  val bootstrapVersion = "5.20.0"
+  val mongoVersion = "0.74.0"
+
   val compile = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % "5.16.0",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"        % "0.68.0",
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % bootstrapVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"        % mongoVersion,
     "com.typesafe.play" %% "play-json-joda"            % "2.6.14"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % "5.16.0",
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-27" % "0.56.0",
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % bootstrapVersion,
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % mongoVersion,
     "org.scalatest"          %% "scalatest"               % "3.2.5", // Note - updating this appears to break flexmark-all
     "org.mockito"            %% "mockito-scala"           % "1.17.12",
     "com.typesafe.play"      %% "play-test"               % current,
