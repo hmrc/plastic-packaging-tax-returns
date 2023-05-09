01029a1be76a0f7eca57d70ff6aa6b3a7776bf8f
“lmcewanhmrc” <lindsay.mcewan@digital.hmrc.gov.uk>
2023-05-09 15:47:44 +0100

    Credits - read first "single-year" claim from user answers

--- test/uk/gov/hmrc/plasticpackagingtaxreturns/models/MultiYearCreditClaimSpec.scala
+++ test/uk/gov/hmrc/plasticpackagingtaxreturns/models/MultiYearCreditClaimSpec.scala
@@ -19,67 +19,50 @@ package uk.gov.hmrc.plasticpackagingtaxreturns.models
 import org.mockito.MockitoSugar
 import org.scalatest.BeforeAndAfterEach
 import org.scalatestplus.play.PlaySpec
+import play.api.libs.json.Json.obj
 import play.api.libs.json.{JsPath, Json, OFormat}
 import uk.gov.hmrc.plasticpackagingtaxreturns.models.cache.UserAnswers
 
 import java.time.LocalDate
 
-case class SingleYearClaim(endDate: LocalDate, exportedCredits: CreditsAnswer, convertedCredits: CreditsAnswer)
+case class SingleYearClaim(endDate: LocalDate, exportedCredits: Option[CreditsAnswer], convertedCredits: Option[CreditsAnswer])
 
 object SingleYearClaim {
-  implicit val formats: OFormat[SingleYearClaim] = Json.format[SingleYearClaim]
+  
+  def readFirstFrom(userAnswers: UserAnswers): SingleYearClaim =
+    userAnswers
+      .getOrFail[Map[String, SingleYearClaim]](JsPath \ "credit")
+      .values
+      .head
 
+  implicit val formats: OFormat[SingleYearClaim] = Json.format[SingleYearClaim]
 }
 
 class MultiYearCreditClaimSpec extends PlaySpec with MockitoSugar with BeforeAndAfterEach {
-  private val asString =
-    """
-    {
-      "_id" : "Int-165fb8fa-20fe-419c-8c17-3f142f36423c-XMPPT0000000003",
-      "data" : {
-          "obligation" : {
-              "fromDate" : "2023-01-01",
-              "toDate" : "2023-03-31",
-              "dueDate" : "2023-05-31",
-              "periodKey" : "23C1"
-          },
-          "isFirstReturn" : false,
-          "startYourReturn" : true,
-          "whatDoYouWantToDo" : true,
-          "credit" : {
-              "2023-04-01-2024-03-31" : {
-                  "endDate" : "2024-03-31",
-                  "exportedCredits" : {
-                      "yesNo" : true,
-                      "weight" : 12344
-                  },
-                  "convertedCredits" : {
-                      "yesNo" : true,
-                      "weight" : 123
-                  }
-              },
-              "2023-01-01-2023-03-31" : {
-                  "endDate" : "2023-03-31",
-                  "exportedCredits" : {
-                      "yesNo" : false
-                  },
-                  "convertedCredits" : {
-                      "yesNo" : true,
-                      "weight" : 99
-                  }
-              }
-          }
-      },
-      "lastUpdated" : {"$date":{"$numberLong":"0"}}
+  
+  "it" should {
+    "read from user answers" in {
+      val userAnswers = UserAnswers("id", obj(
+        "credit" -> obj(
+          "2023-04-01-2024-03-31" -> obj(
+            "endDate" -> "2024-03-31",
+            "exportedCredits" -> obj(
+              "yesNo" -> true,
+              "weight" -> 12344
+            ),
+            "convertedCredits" -> obj(
+              "yesNo" -> true,
+              "weight" -> 123
+            )
+          ),
+        )
+      ))
+
+      SingleYearClaim.readFirstFrom(userAnswers) mustBe SingleYearClaim(
+        LocalDate.of(2024, 3, 31),
+        Some(CreditsAnswer(true, Some(12344L))),
+        Some(CreditsAnswer(true, Some(123L))),
+      )
     }
-    """
-    
-  "it should read from json via UserAnswers" in {
-    val userAnswers = Json.parse(asString).as[UserAnswers]
-    userAnswers.getOrFail[Map[String, SingleYearClaim]](JsPath \ "credit").values.head mustBe SingleYearClaim(
-      LocalDate.of(2024, 3, 31),
-      CreditsAnswer(true, Some(12344L)), 
-      CreditsAnswer(true, Some(123L)), 
-    )
   }
 }
