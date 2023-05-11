aa2e83b4945c81d54b5dadd3401c57a59adaa2ed
mycp98 <marcycalderon@hotmail.co.uk>
2023-05-11 09:43:16 +0100

    tweaked hard-coded available year

--- app/uk/gov/hmrc/plasticpackagingtaxreturns/services/AvailableCreditDateRangesService.scala
+++ app/uk/gov/hmrc/plasticpackagingtaxreturns/services/AvailableCreditDateRangesService.scala
@@ -8,7 +8,7 @@ class AvailableCreditDateRangesService {
   //todo: actually do something
 
   def get = Seq(
-    CreditRangeOption(LocalDate.of(2023, 1, 1), LocalDate.of(2023, 3, 31)),
+    CreditRangeOption(LocalDate.of(2023, 4, 1), LocalDate.of(2023, 3, 31)),
     CreditRangeOption(LocalDate.of(2023, 4, 1), LocalDate.of(2024, 3, 31)),
   )
 
