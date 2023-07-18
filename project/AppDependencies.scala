38fd6dd20dfd43e29535f24f3482afb751ef7c98
Marcy Calderón <97286511+mycp98@users.noreply.github.com>
2023-07-18 16:07:51 +0100

    PPTP-2957 Live handover update dependencies (#196)
    * updated dependencies
    
    * removed some config keys

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,8 +3,8 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapVersion = "7.15.0"
-  val mongoVersion = "0.74.0"
+  val bootstrapVersion = "7.19.0"
+  val mongoVersion = "1.3.0"
 
   val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % bootstrapVersion,
@@ -13,7 +13,7 @@ object AppDependencies {
 
   val test = Seq(
 
-    "org.scalatestplus"      %% "scalacheck-1-15" % "3.2.10.0",
+    "org.scalatestplus"      %% "scalacheck-1-15"         % "3.2.10.0",
     "org.mockito"            %% "mockito-scala-scalatest" % "1.17.14",
     "org.scalatestplus"      %% "mockito-4-11"            % "3.2.16.0",
     "org.scalatestplus.play" %% "scalatestplus-play"      % "5.1.0",
@@ -26,7 +26,7 @@ object AppDependencies {
     "com.vladsch.flexmark"    % "flexmark-all"            % "0.36.8", // Note - updating this requires newer version of JRE
     "org.scalatestplus.play" %% "scalatestplus-play"      % "5.1.0",
     "com.github.tomakehurst"  % "wiremock-jre8"           % "2.26.3",
-    "com.vladsch.flexmark"    % "flexmark-all"                % "0.62.2"
+    "com.vladsch.flexmark"    % "flexmark-all"            % "0.62.2"
   ).map(_ % "test, it")
 
 }
