3ad53529f9228c4be46a0b3e35c44e1097b0df8c
NabeelK102 <NabeelK102@users.noreply.github.com>
2025-08-01 16:10:32 +0100

    DDCYLS-7070: upgrade dependencies and jakson core

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,8 +3,8 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapVersion = "8.6.0"
-  val mongoVersion     = "2.6.0"
+  val bootstrapVersion = "9.18.0"
+  val mongoVersion     = "2.7.0"
   val playVersion      = "30"
 
   val compile = Seq(
@@ -19,12 +19,12 @@ object AppDependencies {
     "org.scalatestplus.play" %% "scalatestplus-play"                 % "5.1.0",
     "uk.gov.hmrc"            %% s"bootstrap-test-play-$playVersion"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"      %% s"hmrc-mongo-test-play-$playVersion" % mongoVersion,
-    "org.scalatest"          %% "scalatest"                          % "3.2.15", // Note - updating this appears to break flexmark-all
-    "org.mockito"            %% "mockito-scala"                      % "1.17.12",
-    "org.playframework"      %% "play-test"                          % current,
-    "org.scalatestplus"      %% "scalatestplus-mockito"              % "1.0.0-M2",
-    "com.vladsch.flexmark"    % "flexmark-all"                       % "0.64.6", // Note - updating this requires newer version of JRE
-    "org.scalatestplus.play" %% "scalatestplus-play"                 % "5.1.0"
+    "org.scalatest"       %% "scalatest"             % "3.2.15", // Note - updating this appears to break flexmark-all
+    "org.mockito"         %% "mockito-scala"         % "1.17.12",
+    "org.playframework"   %% "play-test"             % current,
+    "org.scalatestplus"   %% "scalatestplus-mockito" % "1.0.0-M2",
+    "com.vladsch.flexmark" % "flexmark-all"          % "0.64.6", // Note - updating this requires newer version of JRE
+    "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0"
   ).map(_ % Test)
 
 }
