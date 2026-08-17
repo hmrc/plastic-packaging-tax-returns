8256e473480b833c34da6bcbc0641700af368715
lewis-nicholls <173899555+lewis-nicholls@users.noreply.github.com>
2026-08-17 08:55:59 +0100

    updating mongo version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import sbt._
 object AppDependencies {
 
   val bootstrapVersion = "10.7.0"
-  val mongoVersion     = "2.12.0"
+  val mongoVersion     = "2.13.0"
   val playVersion      = "30"
 
   val compile = Seq(
